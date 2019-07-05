2dd36916b21b9b5e89180db33507f9b4a11a947c
zhuxiaopeng <zhuxiaopeng@zbj.com>
2019-07-05 13:38:04 +0800

    create python robot

---
+++ README.md
@@ -0,0 +1,19 @@
+# let us learn python3
+
+## Homebrew install python3
+
+```bash
+brew install python3
+```
+
+Mac 更新系统后可能 Homebrew 就不正常了，重新安装即可
+
+## run python3
+```bash
+python3 xxx.py
+```
+
+## 交互模式
+```bash
+python3.*
+```
---
+++ Robot.py
@@ -0,0 +1,72 @@
+import schedule
+import time
+import requests
+import json
+
+
+class Robot(object):
+
+    def __init__(self, name='Robot', schedule=[]):
+        self.name = name
+        self.schedule = schedule
+
+    def tell(self, webhook):
+        self.webhook = webhook
+        return self
+
+    def when(self, time):
+        self.time = time
+        return self
+
+    def do(self, something):
+        self.schedule.append({'time': self.time, 'task': something})
+        for i in self.schedule:
+            if(self.time == i['time']):
+                i['task'] = something
+        return self
+
+    def job(self, task):
+        print(task)
+        request = requests.session()
+        url = self.webhook
+        headers = {
+            'Content-Type': 'application/json',
+        }
+        req = request.post(
+            url,
+            data=json.dumps(task),
+            headers=headers
+        )
+        print(req.text)
+
+    def start(self):
+        print(self.name, self.schedule, self.webhook)
+
+        for item in self.schedule:
+            print(item['time'])
+            schedule.every().day.at(item['time']).do(
+                self.job, item['task']
+            )
+
+
+Atom = Robot('Atom')
+Atom.tell(
+    'https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=833dfc59-e225-4965-b1f8-da3dbd88e8af'
+).when('13:30').do(
+    {
+        'msgtype': "text",
+        'text': {
+                'content': 'Hello World From Python Robot 30🤖️'
+        }
+    }
+).when('13:31').do(
+    {
+        'msgtype': "text",
+        'text': {
+                'content': 'Hello World From Python Robot 31🤖️'
+        }
+    }
+).start()
+while True:
+    schedule.run_pending()
+    time.sleep(1)
